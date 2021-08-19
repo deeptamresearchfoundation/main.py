b846b5126c42558fe429129332f92c533fd5c9be
deeptamresearchfoundation <80377220+deeptamresearchfoundation@users.noreply.github.com>
2021-08-19 20:49:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+from os import write
 import pyautogui
 import time
 from inflect import engine
 time.sleep(2)
-for x in range(375001, 395000):
+for x in range(375001, 375999):
     n=x
     p = engine()
     num = p.number_to_words(n)
@@ -10,5 +11,12 @@ for x in range(375001, 395000):
         ret = "true"
     else:
         ret = "false"
-    pyautogui.write("else if ( number === " + str(x) + " || number === \"" + str(x) + "\"" + " || number === \"" + num.replace(",", "").lower() + "\" || number === \"" + num.replace(",", "").title() +"\" || number === \"" + num.replace(",", "").upper() + "\") return " + ret + ";\n")
-    time.sleep(1)
+    pyautogui.write("else if")
+    pyautogui.write("(number === ")
+    pyautogui.write(str(x))
+    pyautogui.write(" || number === \"")
+    pyautogui.write(str(x))
+    pyautogui.write("\" || number === ")
+    time.sleep(0.004)
+    pyautogui.write("\"" + num.replace(",", "").lower() + "\" || number === \"" + num.replace(",", "").title() +"\" || number === \"" + num.replace(",", "").upper() + "\") return " + ret + ";\n    ")
+    time.sleep(0.1)
